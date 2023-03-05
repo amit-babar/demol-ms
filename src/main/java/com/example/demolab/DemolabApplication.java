154bfaadd92e95966259d1c86c79d88ae7458d12
unknown <amit.babar@gmail.com>
2023-03-05 09:12:12 +0530

    initial commit

---
+++ src/main/java/com/example/demolab/DemolabApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demolab/DemolabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
